f5ee830c1805ac8bb13facdcf113066daa5355e1
hkdas5464 <hkdas5464@gmail.com>
2020-04-17 23:51:15 +0530

    Updates

---
+++ precache-manifest.e1c451d05d5328c68f359535ed813971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56477f143528f151ef7c324e43fe76b1",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "826205cc7fca68d94f68",
+    "url": "/note-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ce9991913d95625ef989",
+    "url": "/note-app/static/css/main.fff5059d.chunk.css"
+  },
+  {
+    "revision": "826205cc7fca68d94f68",
+    "url": "/note-app/static/js/2.fd168d2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/note-app/static/js/2.fd168d2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9991913d95625ef989",
+    "url": "/note-app/static/js/main.157d6f27.chunk.js"
+  },
+  {
+    "revision": "e6cda9e696187e5364f6",
+    "url": "/note-app/static/js/runtime-main.07f4dc8b.js"
+  }
+]);
